e33e15ec17c5824e945aa9cf5692ab5009e1f67c
JasonEb <jasonebueng@gmail.com>
2020-07-31 16:52:37 -0700

    adds login api route

--- routes/api/users.js
+++ routes/api/users.js
@@ -36,4 +36,26 @@ router.post('/register', (req, res) => {
 
 })
 
+//Login
+router.post('/login', (req, res) =>{
+    const {email, password} = req.body
+
+    User.findOne({email})
+        .then( user => {
+            if (!user) {
+                return res.status(400).json({email: 'User does not exist'})
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({msg: 'Success'})
+                    } else {
+                        return res.status(400).json({password: 'Incorrect password'})
+                    }
+                })
+        })
+
+})
+
 module.exports = router;
