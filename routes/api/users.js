69c5fdca3974c976189bce7dfe0c646e421ba2a4
JasonEb <jasonebueng@gmail.com>
2020-08-03 18:54:13 -0700

    Adds validations
    implements validation into api routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,11 +6,20 @@ const keys = require('../../config/keys')
 const passport = require('passport')
 const User = require('../../models/User')
 
+//Validations
+const validateRegisterInput = require('../../validations/register')
+const validateLoginInput = require('../../validations/login')
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }))
 
 //Register
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body)
+
+    if (!isValid) {
+        return res.status(400).json(errors)
+    }
+
     // Check for duplicate email
     User.findOne({ email: req.body.email})
         .then( user => {
@@ -49,6 +58,12 @@ router.post('/register', (req, res) => {
 
 //Login
 router.post('/login', (req, res) =>{
+    const { errors, isValid } = validateLoginINput(req.body)
+
+    if (!isValid) {
+        return res.status(400).json(errors)
+    }
+ 
     const {email, password} = req.body
 
     User.findOne({email})
