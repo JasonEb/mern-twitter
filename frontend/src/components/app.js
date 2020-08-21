e3c71e080e39d41c53945942ce6f16de352d4da8
JasonEb <jasonebueng@gmail.com>
2020-08-21 16:54:14 -0700

    adds hooks version of tweet form

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -11,6 +11,7 @@ import SignupFormContainer from './session/signup_form_container'
 import TweetsContainer from './tweets/tweets_container'
 import Profile from './profile/profile'
 import TweetFormContainer from './tweets/tweet_form_container'
+import TweetFormHooks from './tweets/tweet_form_hooks'
 
 const App = () => (
     <div>
@@ -22,7 +23,7 @@ const App = () => (
 
             <ProtectedRoute exact path="/tweets" component={TweetsContainer} />
             <ProtectedRoute exact path="/profile" component={Profile} />
-            <ProtectedRoute exact path="/new_tweet" component={TweetFormContainer} />
+            <ProtectedRoute exact path="/new_tweet" component={TweetFormHooks} />
         </Switch>
     </div>
 )
---
+++ frontend/src/components/tweets/tweet_form_hooks.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import Tweet from './tweet'
+import { composeTweet } from '../../actions/tweet_actions'
+
+const TweetFormHooks = () => {
+    const [text, setText] = useState('')
+    const dispatch = useDispatch()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        dispatch(composeTweet(text))
+    }
+
+    const update = (e) => {
+        setText(e.target.value)
+    }
+
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <input 
+                        type="textarea"
+                        value={text}
+                        onChange={update}
+                        placeholder="Write message..." 
+                    />
+                    <input type="submit" value="Submit" />
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default TweetFormHooks
